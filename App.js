505af865ce5615c7491fc990e0de46b3dffd90e5
rafaelrmacedo <rafaelrodriguesmacedo2004@gmail.com>
2022-12-21 11:06:06 -0300

    first react files

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './assets/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <div id="wrapper">
+        <div id="time">
+          <p id="timern"></p>
+        </div>
+        <div id="header">
+        </div>
+        <div id="content">
+          <div id="title">
+            <p id="titletext">Rodolfinhogamer12</p>
+          </div>
+          <div id="addbar">
+            <button id="addser">Adicionar Série</button>
+          </div>
+          <div id="addedser"></div>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default App;
